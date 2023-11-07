76b11230acae61928bed2a8f7f78b48d4802d63d
Shakhzoddev538 <sahadeveloper809@gmail.com>
2023-11-07 14:27:36 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let cartinka1 = document.querySelector('.circle1')
+let cartinka2 = document.querySelector('.circle2')
+let cartinka3 = document.querySelector('.circle3')
+let cartinka4 = document.querySelector('.circle4')
+
+let cartinka = document.querySelector('.left__main')
+
+cartinka1.onclick = () => {
+    cartinka.classList.add('img1')
+}
+cartinka2.onclick = () => {
+    cartinka.classList.add('img2')
+}
+cartinka3.onclick = () => {
+    cartinka.classList.add('img3')
+}
+cartinka4.onclick = () => {
+    cartinka.classList.add('img4')
+}
